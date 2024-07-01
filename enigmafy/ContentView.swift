83ccddbc23bf65491e0320852e95691f3ddb2ed9
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-07-01 08:10:00 -0700

    Initial Commit

---
+++ enigmafy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  enigmafy
+//
+//  Created by Mohsen Bakhit on 2024-07-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ enigmafy/enigmafyApp.swift
@@ -0,0 +1,17 @@
+//
+//  enigmafyApp.swift
+//  enigmafy
+//
+//  Created by Mohsen Bakhit on 2024-07-01.
+//
+
+import SwiftUI
+
+@main
+struct enigmafyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
